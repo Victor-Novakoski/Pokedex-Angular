df463f7859d9f399065a6e6028e0b28ffa32b253
Victor Novakoski <Novakoski2021@gmail.com>
2023-06-11 16:25:32 -0300

    fixed:add pokemon list

---
+++ angular-pokedex/src/app/pokemon-list/pokemon-list.component.ts
@@ -0,0 +1,11 @@
+import { PokemonService } from './../services/pokemon.service';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pokemon-list',
+  templateUrl: './pokemon-list.component.html',
+  styleUrls: ['./pokemon-list.component.sass'],
+})
+export class PokemonListComponent {
+  constructor(public PokemonService: PokemonService) {}
+}
